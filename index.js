2eaac4be52c0aecb50d3e56e3e7446909076b28a
kathryn0908 <kathrynlael@gmail.com>
2020-05-06 09:38:02 -0600

    Updated post and search forms

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-fetch("http://localhost:3000/fish")
-.then(response => response.json())
-.then(displayFish)
+let fishUrl;
+const queryParams = new URLSearchParams(window.location.search);
+const search = queryParams.get('search');
 
-    function displayFish(fishinfo) {
-        fishinfo.forEach(fish => {
-            const cardContainer = document.querySelector("#card-container")
-            const species_illustration_photo = document.createElement('img')
+if(search) {
+    fishUrl = `http://localhost:3000/fish?search=${search}`
+}
+else { 
+    fishUrl = "http://localhost:3000/fish"
+}
 
-            cardContainer.innerHTML = cardContainer.innerHTML + `<a href=show.html?id=${fish.id}<button>${fish.species_name}</button></a>`
 
-            if (fish.species_illustration_photo) {
-            species_illustration_photo.src = fish.species_illustration_photo
-            } else {
-                noImage.textContent = 'No image available'
-            }
-           cardContainer.append(species_illustration_photo)
-        })
-    }
 
-    // function displayFishData(fish){
-    //     fish.forEach(fish => {
-    //         const h5 = document.createElement('h5')
-    //         const species_illustration_photo = document.createElement('img')
-    //         const p = document.createElement('p')
-    //         h5.innerHTML = fish.species_name
-    //         species_illustration_photo.innerHTML = fish.species_illustration_photo
-    //         p.innerHTML = fish.habitat
-    //         document.body.append(h5, species_illustration_photo, p)
-    //        
+fetch(fishUrl)
+    .then(response => response.json())
+    .then(displayFish)
+
+function displayFish(fishinfo) {
+    console.log(fishinfo)
+    fishinfo.forEach(fish => {
+        const cardContainer = document.querySelector(".card-container")
+        const species_illustration_photo = document.createElement('img')
+
+        cardContainer.innerHTML = cardContainer.innerHTML + `<a href=show.html?id=${fish.id}><button>${fish.species_name}</button></a>`
+
+        if (fish.species_illustration_photo) {
+        species_illustration_photo.src = fish.species_illustration_photo
+        } else {
+            // noImage.textContent = 'No image available'
+        }
+
+        searchFish(fish)
+        
+        cardContainer.append(species_illustration_photo)
+    })
+}
+
+function searchFish(fish) { 
+    const $li = createElement("li")
+    $li.innerHTML = `<a href=show.html?id=${fish.id}><button>${fish.species_name}</button></a>`
+    cardContainer.append($li)
+    }  
